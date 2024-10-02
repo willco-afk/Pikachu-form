7505c9ab98d5e0408bd48d7c32f4fa4050baf4ae
willco-afk <131642549+willco-afk@users.noreply.github.com>
2024-10-02 20:48:21 +0800

    Your commit message here

--- mynewproject/settings.py
+++ mynewproject/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 # Application definition
 INSTALLED_APPS = [
